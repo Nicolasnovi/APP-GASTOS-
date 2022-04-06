4ba8b09daf048e5b2208fab7fe874856ae2ce337
Nicolas Novi <niconovi70@gmail.com>
2022-04-06 20:02:15 -0300

    cree la clase movimientos,  la subi a la base de datos y agregue la appGastos a settings

--- ProyectoAppGastos/AppGastos/models.py
+++ ProyectoAppGastos/AppGastos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class movimientos(models.Model):
+    Fecha= models.CharField('Fecha', max_length=50)
+    Categoria= models.CharField('Categoria', max_length=50)
+    Importe= models.IntegerField()
+    Nota= models.CharField('Nota', max_length=50)
+    Tipo_gasto= models.CharField('Tipo de Gasto', max_length=50)
+
